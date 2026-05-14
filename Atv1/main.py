051f1497373a520323e90d50a961a4c0da935d75
Ninassc <ninasepulvedacarvalho@gmail.com>
2026-05-14 08:54:27 -0300

    second commit

--- Atv1/main.py
+++ Atv1/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('./index.html')
+    return " Um decorator (decorador) em Python é uma funcionalidade poderosa e elegante que permite modificar ou estender o comportamento de funções, métodos ou classes sem alterar o seu código fonte original.Decorators são ideais para resolver problemas transversais (cross-cutting concerns) — lógicas que precisam ser aplicadas em múltiplos lugares, mas que não fazem parte da lógica de negócios principal da função"
 
 if __name__ == "__main__":
     app.run(debug=True)
