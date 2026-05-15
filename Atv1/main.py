05950374b2cbef3de4fc28f21291958ea55961cb
Nina Sepúlveda <ninasepulvedacarvalho@gmail.com>
2026-05-15 08:17:13 -0300

    Add route

--- Atv1/main.py
+++ Atv1/main.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/decorator")
 def index():
     return " Um decorator (decorador) em Python é uma funcionalidade poderosa e elegante que permite modificar ou estender o comportamento de funções, métodos ou classes sem alterar o seu código fonte original.Decorators são ideais para resolver problemas transversais (cross-cutting concerns) — lógicas que precisam ser aplicadas em múltiplos lugares, mas que não fazem parte da lógica de negócios principal da função"
 
