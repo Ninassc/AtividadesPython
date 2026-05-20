6181da3a19c624a410075346e0bf72967a768161
Nina <ninasepulvedacarvalho@gmail.com>
2026-05-20 20:44:58 -0300

    mais paginas

--- Atv4/app.py
+++ Atv4/app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/gatos")
+def gatos():
+    return render_template('gatos.html')
+
+@app.route("/cachorros")
+def cachorros():
+    return render_template('cachorros.html')
+
+@app.route("/oncas")
+def oncas():
+    return render_template('oncas.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
