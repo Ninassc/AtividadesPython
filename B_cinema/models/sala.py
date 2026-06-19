d25c5473ddcfa278c876ff02ffca7d23b01df4e7
Ninassc <ninasepulvedacarvalho@gmail.com>
2026-06-19 08:58:04 -0300

    atv cinema

---
+++ B_cinema/models/sala.py
@@ -0,0 +1,16 @@
+from . import db
+from .base import ModeloBase
+
+
+class Sala(ModeloBase):
+    __tablename__ = "salas"
+
+    numero = db.Column(db.Integer, nullable=False)
+    capacidade = db.Column(db.Integer, nullable=False)
+
+    # TODO ALUNO: relationship sessoes
+    sessoes = db.relationship("Sessao", back_populates="filmes")
+
+    @classmethod
+    def listar(cls):
+        return cls.query.order_by(cls.numero).all()
